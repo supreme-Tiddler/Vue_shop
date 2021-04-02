3403716eb479a4b75d86f505f6d2316e33545e3f
xiaokangyyu <yzk2249665664@qq.com>
2021-04-02 18:38:51 +0800

    portion HOME

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,74 @@
 <template>
-  <div>
-    <el-button type="info" @click="loginout">退出</el-button>
-  </div>
+  <el-container class="home_container">
+    <!-- 头部区域 -->
+    <el-header>
+      <!-- 标题 -->
+      <span>电商后台管理系统</span>
+      <el-button type="info" @click="loginout">退出</el-button>
+    </el-header>
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse?'64px':'200px'">
+        <!-- 收缩导航的按钮 -->
+        <div class="toggle_button" @click="toggleCollapse">| | |</div>
+        <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="rgb(49,53,65)"
+        text-color="#fff"
+        unique-opened
+        :collapse="isCollapse"
+        active-text-color="rgb(64,158,255)">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
+            <!-- 一级菜单模板 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+              <!-- 二级菜单 -->
+              <el-menu-item :index="subItem.id+''" v-for="subItem in item.children" :key="subItem.id">
+                 <!-- 二级菜单模板 -->
+                 <template slot="title">
+                    <!-- 二级图标 -->
+                    <i class="el-icon-menu"></i>
+                    <!--二级 文本 -->
+                    <span>{{subItem.authName}}</span>
+                  </template>
+              </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主题 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
   name: 'Hoem',
+  data () {
+    return {
+      menuList: null,
+      iconsObj: {
+        125: 'el-icon-s-custom',
+        103: 'el-icon-mouse',
+        101: 'el-icon-wallet',
+        102: 'el-icon-postcard',
+        145: 'el-icon-pie-chart'
+      },
+      // 默认打开侧边菜单栏
+      isCollapse: false
+    }
+  },
+  // 当vue实例已经创建后
+  created () {
+    this.getMenuList()
+  },
   // 登出 清空 token 后跳转到登录页
   methods: {
     loginout () {
@@ -14,10 +77,54 @@ export default {
       // 跳转到登录页
       this.$router.push('/login')
       this.$message.success('退出成功')
+    },
+    // 当vue实例已经创建后 需要调用的函数 获取菜单数据
+    async getMenuList () {
+      // 获取数据并解构
+      // 解构对象 得到data 变量 并且使用别名 res
+      const { data: res } = await this.$http.get('menus')
+      // console.log(res)//后台请求过来的数据
+      // 如果获取列表失败,直接结束函数,并且提示错误
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取数据失败')
+      }
+      // 将数据存储,用于刷新页面
+      this.menuList = res.data
+      console.log(this.menuList)
+    },
+    // 收缩侧边栏
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
 </script>
 <style lang="scss">
-
+  .home_container{
+    height: 100%;
+  }
+  .el-header{
+    background-color: rgb(55,61,65);
+    color: #fff;
+    font-size: 20px;
+    display: flex;
+    justify-content: space-between;// 两端对齐
+    align-items: center;
+  }
+  .el-aside{
+    background-color: rgb(49,53,65);
+    .el-menu{
+      border-right: none;
+    }
+  }
+  .el-main{
+    background-color: rgb(234, 237, 241);
+  }
+  .toggle_button{
+    background-color: rgb(74 , 80, 100);
+    text-align: center;
+    padding: 5px 0;
+    color: #fff;
+    cursor: pointer;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 // 设置基准路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//  每个请求都需要带上 token,因为这个token用于校验是否登录
+axios.interceptors.request.use(config => {
+  // 读取sessionStorage 中保存的token值 , 作为Authorization这个字段 传到服务器
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
 // 因为多个组件需要发起请求,所以将axios挂载到vue函数中
 // 挂载在vue的原型上 所有的vue实例都有http方法
 Vue.prototype.$http = axios
